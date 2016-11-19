f23041f65cf76b2cce75fe1b87866d89ffa959c9
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-11-19 13:24:47 +0300

    no message

--- T/Main.php
+++ T/Main.php
@@ -1,10 +1,16 @@
 <?php
 // 2016-11-19
-namespace Dfe\BlackbaudNetCommunity;
+namespace Dfe\BlackbaudNetCommunity\T;
+use Dfe\BlackbaudNetCommunity\Settings as S;
 class Main extends \Df\Core\TestCase {
+	/**
+	 * 2016-11-19
+	 */
+	public function t00() {}
+
 	/**
 	 * @test
 	 * 2016-11-19
 	 */
-	public function t00() {}
+	public function t01() {echo S::s()->privateKey();}
 }
