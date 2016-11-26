95691512cdb3edeba0a7589039172f1b492edd35
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-11-26 23:35:56 +0300

    0.0.8

--- Button.php
+++ Button.php
@@ -3,11 +3,20 @@
 namespace Dfe\BlackbaudNetCommunity;
 class Button extends \Df\Sso\Button {
 	/**
-	 * 2016-11-23
+	 * 2016-11-26
 	 * @override
-	 * @see \Df\Sso\Button::loggedOut()
-	 * @used-by \Df\Sso\Button::_toHtml()
+	 * @see \Df\Sso\Button::htmlL()
+	 * @used-by \Df\Sso\Button::html()
 	 * @return string
 	 */
-	protected function loggedOut() {return df_tag_a($this->s()->label(), Url::get());}
+	protected function htmlL() {return df_tag_a($this->s()->label(), Url::get());}
+
+	/**
+	 * 2016-11-26
+	 * @override
+	 * @see \Df\Sso\Button::htmlU()
+	 * @used-by \Df\Sso\Button::html()
+	 * @return string
+	 */
+	protected function htmlU() {return '';}
 }
