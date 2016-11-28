dc4bf77c02e1ead6c04c59f40de689a158d9f7bf
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-11-29 00:02:46 +0300

    0.8.1

--- Button.php
+++ Button.php
@@ -3,15 +3,6 @@
 namespace Dfe\BlackbaudNetCommunity;
 class Button extends \Df\Sso\Button {
 	/**
-	 * 2016-11-26
-	 * @override
-	 * @see \Df\Sso\Button::htmlU()
-	 * @used-by \Df\Sso\Button::html()
-	 * @return string
-	 */
-	protected function htmlU() {return '';}
-
-	/**
 	 * 2016-11-27
 	 * @override
 	 * @see \Df\Sso\Button::lHref()
