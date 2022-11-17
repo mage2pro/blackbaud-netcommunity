d88d9e61e80946212233cf8e94d8a9a39f9c486d
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-17 16:11:49 +0000

    https://github.com/mage2pro/core/issues/168

--- Settings.php
+++ Settings.php
@@ -1,21 +1,19 @@
 <?php
-# 2016-11-19
 namespace Dfe\BlackbaudNetCommunity;
+# 2016-11-19
 # Аргумент $s для методов этого класса не нужен,
 # потому что опции этого класса считывается только на витрине для текущего магазина.
 /** @method static Settings s() */
 final class Settings extends \Df\Sso\Settings {
 	/**
 	 * 2016-11-19
-	 * @return string
 	 */
-	function privateKey() {return $this->p();}
+	function privateKey():string {return $this->p();}
 
 	/**
 	 * 2016-11-20
-	 * @return string
 	 */
-	function url() {return $this->v();}
+	function url():string {return $this->v();}
 
 	/**
 	 * 2016-11-19
